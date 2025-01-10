9cfa64485d32524b651718956acd4edd3b4a8688
Ivan Voloshchuk <keranamv@gmail.com>
2025-01-10 23:00:04 +0100

    Implement: isValidOdometer

--- Sources/Model/SwiftData/FuelExpense.swift
+++ Sources/Model/SwiftData/FuelExpense.swift
@@ -66,3 +66,39 @@ class FuelExpense: Identifiable {
         context.delete(self)
     }
 }
+
+extension FuelExpense {
+    /// Checks if the odometer value is valid for the current fuel expense
+    /// - Parameter vehicle: The vehicle to check the odometer value against
+    /// - Returns: A boolean indicating if the odometer value is valid
+    func isValidOdometer(for vehicle: Vehicle) -> Bool {
+        let sortedExpenses = vehicle.fuelExpenses.sorted { $0.date < $1.date }
+
+        guard sortedExpenses.count > 0 else {
+            return true // No previous expenses, so the odometer value is valid
+        }
+
+        // Binary search for the current fuel expense date
+        guard let index = sortedExpenses.firstIndex(where: { $0.date >= self.date }) else {
+            if let lastExpense = sortedExpenses.last, lastExpense.odometer > self.odometer {
+                return false
+            }
+            return true
+        }
+
+        let closestBefore = index > 0 ? sortedExpenses[index - 1] : nil
+        let closestAfter = index < sortedExpenses.count - 1 ? sortedExpenses[index + 1] : nil
+
+        // Check if the odometer value is not lower than the previous
+        if let before = closestBefore, before.odometer > self.odometer {
+            return false
+        }
+
+        // Check if the odometer value is not higher than the next
+        if let after = closestAfter, after.odometer < self.odometer {
+            return false
+        }
+
+        return true
+    }
+}

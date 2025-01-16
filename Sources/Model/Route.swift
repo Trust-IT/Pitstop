8542dcc7546969f33c3be36a995d610f79ea7e93
Ivan Voloshchuk <keranamv@gmail.com>
2025-01-16 11:03:27 +0100

    Add: Route model

---
+++ Sources/Model/Route.swift
@@ -0,0 +1,39 @@
+//
+//  Route.swift
+//  Pitstop
+//
+//  Created by Ivan Voloshchuk on 16/01/25.
+//
+
+import SwiftUI
+
+enum Route {
+    // Onboarding
+    case onboardingWelcome
+    case onboardingRegistration
+    case onboardingMoreInfo
+    case onboardingNotification
+    case onboardingReady
+
+    // Vehicle
+    case addNewReport
+}
+
+extension Route: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(hashValue)
+    }
+}
+
+extension Route: View {
+    var body: some View {
+        switch self {
+        case .onboardingWelcome:
+            OnbWelcomeView()
+        case .addNewReport:
+            AddReportView()
+        default:
+            EmptyView()
+        }
+    }
+}
